bdf122cbe04f56c587d8b99f5f94e1c855f9ba07
mrsajadpp <mrsajadpp@gmail.com>
2024-10-25 17:02:18 +0530

    login added

--- route/login.js
+++ route/login.js
@@ -1,14 +1,60 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.SECRET_KEY;
 const router = express.Router();
 
+// Middleware
+const verify = async (req, res, next) => {
+    const { token } = req.query;
+    if (!token) return res.status(401).json({ error: 'Access denied' });
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        if (decoded.username != process.env.USERNAME) return res.status(401).json({ error: 'Access denied' });
+        if (decoded.password != process.env.PASSWORD) return res.status(401).json({ error: 'Access denied' });
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid token' });
+    }
+}
+
+const notLogged = async (req, res, next) => {
+    const { token } = req.query;
+    if (!token) return res.redirect("/");
+    next();
+}
+
 // Login page
-router.get("/login", async (req, res, next) => {
+router.get("/login", notLogged, async (req, res, next) => {
     try {
         res.render("login", { title: "Login" });
     } catch (error) {
         console.error(error);
         res.status(500).send("Internal server issue(500)!");
     }
-})
+});
+
+router.post("/login", notLogged, async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+
+        if (!username) return res.send("Username is required!.");
+        if (!password) return res.send("Password is required!.");
+
+        if (username == process.env.USERNAME && password == process.env.PASSWORD) {
+            const token = await jwt.sign(
+                {
+                    username,
+                    password
+                },
+                secretKey
+            );
+            req.session.token = token;
+            return res.redirect("/");
+        } else return res.send("Credentials are not matching!.");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server issue(500)!");
+    }
+});
 
 module.exports = router;
--- route/user.js
+++ route/user.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.SECRET_KEY;
 const router = express.Router();
 
+// Middleware
+const verify = async (req, res, next) => {
+    const { token } = req.query;
+    if (!token) return res.status(401).json({ error: 'Access denied' });
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        if (decoded.username != process.env.USERNAME) return res.status(401).json({ error: 'Access denied' });
+        if (decoded.password != process.env.PASSWORD) return res.status(401).json({ error: 'Access denied' });
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid token' });
+    }
+}
 
 
 module.exports = router;
