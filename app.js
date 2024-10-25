fa83df3645f8b3d6ce5df8c82fc63d9fa94fedbe
mrsajadpp <mrsajadpp@gmail.com>
2024-10-25 16:34:41 +0530

    hbs added

--- app.js
+++ app.js
@@ -1,57 +1,100 @@
-require("dotenv").config();
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const colors = require('colors');
-const UAParser = require('ua-parser-js');
-const app = express();
-const PORT = 3000;
- 
-const accessLogStream = fs.createWriteStream(
-    path.join(__dirname, "access.log"),
-    { flags: "a" }
-); 
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+let handlebars = require('express-handlebars');
+let session = require('express-session');
+let cookieSession = require('cookie-session');
+let fileUpload = require('express-fileupload');
+let favicon = require("serve-favicon");
+const cors = require('cors');
+const compression = require("compression");
+
+var app = express();
 
 
-// Define a custom token for coloring status code
-morgan.token('status', (req, res) => {
-    const status = res.statusCode;
-    let color = status >= 500 ? 'red'    // server error
-        : status >= 400 ? 'yellow' // client error
-            : status >= 300 ? 'cyan'   // redirection
-                : status >= 200 ? 'green'  // success
-                    : 'reset';                 // default
+var hbs = handlebars.create({});
 
-    return colors[color](status);
-});
+var userRouter = require('./route/user');
 
-// Define the custom morgan format
-app.use(
-    morgan((tokens, req, res) => {
-        return [
-            colors.blue(tokens.method(req, res)),
-            colors.magenta(tokens.url(req, res)),
-            tokens.status(req, res),
-            colors.cyan(tokens['response-time'](req, res) + ' ms'),
-        ].join(' ');
-    })
-);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.use(cors());
+app.engine('hbs', handlebars.engine({
+  extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/', partialsDir: __dirname + '/views/partial/', helpers: {
+    ifequal: function (arg1, arg2, options) {
+      return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+    },
+    formatNumber: (number) => {
+      const units = ['K', 'M', 'B', 'T', 'P', 'E'];
+      let unit = '';
+      let num = number;
 
-app.use(morgan("combined", { stream: accessLogStream }));
+      for (let i = units.length - 1; i >= 0; i--) {
+        const size = Math.pow(1000, i + 1);
+        if (number >= size) {
+          num = number / size;
+          unit = units[i];
+          break;
+        }
+      }
 
-// Middleware to parse User-Agent
-app.use((req, res, next) => {
-    const userAgent = req.headers['user-agent'];
-    const parser = new UAParser(userAgent);
-    const device = parser.getDevice();
-    req.device = device;
-    next();
-});
+      return num.toFixed(1).replace(/\.0$/, '') + unit;
+    },
+    formatDate: function (dateString) {
+      const date = new Date(dateString);
+      const options = { weekday: 'short', month: 'short', year: 'numeric' };
+      const formattedDate = date.toLocaleDateString('en-US', options);
+
+      const [weekday, month, year] = formattedDate.split(' ');
+      return `${weekday}, ${month} ${year}`;
+    }
+  }
+}));
+
+// app.use(session({ secret: "@tricbskt@#]$" }));
+app.use(cookieSession({
+  name: 'session',
+  keys: ["@tricbgrvx@#{$"],
+  maxAge: 30 * 24 * 60 * 60 * 1000
+}))
+app.set('view engine', 'hbs');
+
+
+// app.use(compression());
+app.use(compression({ level: 9 }));
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/user/', require('./route/user'));
 
-app.listen(PORT, () => {
-    console.log(`🚀 Listening at http://127.0.0.1:${PORT}/`);
+app.use('/', userRouter);
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+// });
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', { title: err.status, iserror: true, description: "Don't worry, check out our latest arrivals or explore our collection by category.", status: err.status, message: err.status == 404 ? "Sorry but the page you are looking for does not exist, have been removed, name changed, or is temporarily unavailable." : "Something went wrong on our end. Please try again later. Our team has been notified and is working to fix the issue. Thank you for your patience." });
+});
+
+module.exports = app;
