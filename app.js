fe673fd2c9589f87620e190dda4670f6c16b19d4
mrsajadpp <mrsajadpp@gmail.com>
2024-10-25 16:43:05 +0530

    bug fixed

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ var app = express();
 
 var hbs = handlebars.create({});
 
-var userRouter = require('./route/user');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.use(cors());
@@ -79,11 +77,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', userRouter);
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
-// });
+app.use('/', require('./route/user'));
+app.use('/auth', require('./route/login'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
